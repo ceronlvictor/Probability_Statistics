8269a83f797151ef33e2d26b5fc50d0d06e0f3bf
Victor Ceron <ceronloezavictor@gmail.com>
2021-01-15 13:12:19 -0600

    Making script to calculate location measures

--- variance-standard_deviation-Exercise1-1.py
+++ variance-standard_deviation-Exercise1-1.py
@@ -5,7 +5,10 @@
     d)Grafica de los datos 
     e)Media Recortada al 20%
     f)Que representa mejor los datos Media muestral o Media Recortada """
-
+def main(measure):
+    return len(measure)
 
 if __name__ == "__main__":
     measure = [3.4, 2.5, 4.8, 2.9, 3.6, 2.8, 3.3, 5.6, 3.7, 2.8, 4.4, 4.0, 5.2, 3.0, 4.8]
+    print(f'The measuraments are: {measure}')
+    main(measure)
