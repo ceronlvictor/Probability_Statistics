201fe0ae23f96d6e3e17a7958c3955b916e30b8b
Victor Ceron <ceronloezavictor@gmail.com>
2021-01-15 13:08:36 -0600

    Problem 1.1 Description

--- variance-standard_deviation-Exercise1-1.py
+++ variance-standard_deviation-Exercise1-1.py
@@ -1,2 +1,11 @@
+"""Mediciones para el tiempo de Secado en horas de una marca de pintura esmaltada
+    a)Tamaño de la muestra
+    b)Media
+    c)Mediana 
+    d)Grafica de los datos 
+    e)Media Recortada al 20%
+    f)Que representa mejor los datos Media muestral o Media Recortada """
+
+
 if __name__ == "__main__":
     measure = [3.4, 2.5, 4.8, 2.9, 3.6, 2.8, 3.3, 5.6, 3.7, 2.8, 4.4, 4.0, 5.2, 3.0, 4.8]
