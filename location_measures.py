e871e37af933bb7353f395ad83283ad887302502
Victor Ceron <ceronloezavictor@gmail.com>
2021-01-21 12:21:34 -0600

    Labels for each funtion in location_measures

--- location_measures.py
+++ location_measures.py
@@ -4,11 +4,11 @@
 import math 
 import statistics
 
-def average(X):
+def average(X): #Promedio 
     averag = sum(X)/len(X)
     return averag
 
-def mediana(X):
+def mediana(X): #Mediana 
     sorted_List = sorted(X) #Sorted (Ordenar) the measuremens
     List_Len = len(X)
     index = (List_Len - 1)//2
@@ -18,14 +18,14 @@ def mediana(X):
     else:
         return (sorted_List[index] + sorted_List[index + 1]) / 2
 
-def variance(X):
+def variance(X): #Varianza 
     n = len(X) 
     average = sum(X)/n
     suma = [(x - n)**2 for x in X]
     sigma  = sum(suma) / n 
     return  sigma
 
-def standard_deviation(X):
+def standard_deviation(X): #Desviasción estandar 
     n = len(X) 
     average = sum(X)/n
     suma = [(x - n)**2 for x in X]
@@ -34,13 +34,13 @@ def standard_deviation(X):
     return standard_dev
 
 
-def average_recort(X):
+def average_recort(X):  #Media recortada
     X = sorted (X) #Ordenamos el conjunto de datos de menor a mayor 
     #Necesitamos tomar los datos dependiendo del porcentaje que vamos a eliminar 
     #Otra alternativa es eliminar los datos extremos
     return X
 
-def size(X):
+def size(X): #tamaño de la muestra 
     return len(X)
 
 
@@ -49,7 +49,7 @@ if __name__ == "__main__":
     print(X)
     print(f'The average is: {average(X)}')
     print(f'The median is: {mediana(X)}')
-    print(f'THe average recort is {average_recort(X)}')
+    print(f'The average recort is {average_recort(X)}')
     average(X)
     mediana(X)
     average_recort(X)
