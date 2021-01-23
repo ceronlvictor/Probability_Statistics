ea9711339e6a1601ffee64b026d989cc1c06c810
Victor Ceron <ceronloezavictor@gmail.com>
2021-01-23 13:12:22 -0600

    Add the description for the problem 2.1

---
+++ Excersises_Probability/Probability_Exercise_2-1.py
@@ -0,0 +1,9 @@
+"""Liste los elementos de cada uno de los siguientes espacios muestrales
+    a)Conjunto de numeros enteros entre  1 y 50 que son divisibles entre 8
+    b) El conjunto S = {x|x**2 + 4x -5 = 0}
+    c) EL conjunto de resultados cuando se lanza una moneda al aire hasta 
+    que aparecen una cruz o tres caras
+    d) El conjunto de S = {x | x es un continente}
+    e) El conjunto de S = {x | 2x -4 >= 0 y x < 1} """
+
+
