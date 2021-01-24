fd740b4856e05bcf2923f7f04ff35c8975ed3fe4
Victor Ceron <ceronloezavictor@gmail.com>
2021-01-24 12:43:34 -0600

    Implementing Numpy and Spicy for solve the media recort

--- location_measures.py
+++ location_measures.py
@@ -3,6 +3,8 @@
 #We need to do it more efficient 
 import math 
 import statistics
+import numpy as np 
+import scipy.stats  
 
 def average(X): #Promedio 
     averag = sum(X)/len(X)
@@ -10,6 +12,7 @@ def average(X): #Promedio
 
 def mediana(X): #Mediana 
     sorted_List = sorted(X) #Sorted (Ordenar) the measuremens
+    print(sorted_List)
     List_Len = len(X)
     index = (List_Len - 1)//2
     
@@ -30,10 +33,13 @@ def standard_deviation(X): #Desviasción estandar
 
 
 def average_recort(X):  #Media recortada
-    X = sorted (X) #Ordenamos el conjunto de datos de menor a mayor 
-    #Necesitamos tomar los datos dependiendo del porcentaje que vamos a eliminar 
-    #Otra alternativa es eliminar los datos extremos
-    return X
+    liminf = scipy.stats.scoreatpercentile(X,20)
+    limsup = scipy.stats.scoreatpercentile(X,80)        #Ordenamos el conjunto de datos de menor a mayor 
+    print(f'Limite Inferior {liminf} Limite superior {limsup}')
+    trimean = scipy.stats.mstats.tmean(X,(2.8,5.4))
+    print(f'The recort average is {trimean}')
+    #trimean = scipy.stats.mstats.tmean(X,(108.2,136.025)    #Necesitamos tomar los datos dependiendo del porcentaje que vamos a eliminar 
+        #Otra alternativa es eliminar los datos extremos
 
 def size(X): #tamaño de la muestra 
     return len(X)
@@ -42,7 +48,9 @@ def size(X): #tamaño de la muestra
 if __name__ == "__main__":
     X = [3.4, 2.5, 4.8, 2.9, 3.6, 2.8, 3.3, 5.6, 3.7, 2.8, 4.4, 4.0, 5.2, 3.0, 4.8]
     print(X)
-    print(f'The average is: {average(X)}')
-    print(f'The median is: {mediana(X)}')
-    print(f'The average recort is {average_recort(X)}')
-    print(f'The variance is: {variance(X)}')
+    average_recort(X)
+    mediana(X)
+    #print(f'The average is: {average(X)}')
+    #print(f'The median is: {mediana(X)}')
+    #print(f'The average recort is {average_recort(X)}')
+    #print(f'The variance is: {variance(X)}')
--- variance-standard_deviation-Exercise1-13.py
+++ variance-standard_deviation-Exercise1-13.py
@@ -2,9 +2,18 @@
 el tiempo de vida de cierto tipo de batería. Una muestra en horas de vida 
 es como la siguiente 
         
-            123 116 122 110 175 126 125 111 118 117 
+            123, 116, 122, 110, 175, 126, 125, 111, 118, 117, 
     
     a)Calcule la media y la mediana de la muestra 
     
     b)Qué característica en este conjunto de datos es la responsable de 
         la diferencia sustancial entre ambas """
+
+from location_measures import average, mediana
+
+if __name__=='__main__':
+    time_life = [123, 116, 122, 110, 175, 126, 125, 111, 118, 117]
+    print(f'The sample is: {time_life}')
+    print(f'The Average is: {average(time_life)}')
+    print(f'The median is: {mediana(time_life)}')
+    print(f'The diference is because the extreme values afect the average')
