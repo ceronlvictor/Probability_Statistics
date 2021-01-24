c598b101bd8f411582b19781e85d3066460b4884
Victor Ceron <ceronloezavictor@gmail.com>
2021-01-24 11:58:44 -0600

    Add the description of the problem 2.17

---
+++ variance-standard_deviation-Exercise1-13.py
@@ -0,0 +1,10 @@
+""" Un fabricante de componentes electrónicos se interesa en determinar 
+el tiempo de vida de cierto tipo de batería. Una muestra en horas de vida 
+es como la siguiente 
+        
+            123 116 122 110 175 126 125 111 118 117 
+    
+    a)Calcule la media y la mediana de la muestra 
+    
+    b)Qué característica en este conjunto de datos es la responsable de 
+        la diferencia sustancial entre ambas """
