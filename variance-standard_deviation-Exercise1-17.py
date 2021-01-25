7fb40ec368db8f22d284c45ac21941311eccb49e
Victor Ceron <ceronloezavictor@gmail.com>
2021-01-25 12:07:58 -0600

    Add the solution for a) of the problem 1.17

--- variance-standard_deviation-Exercise1-17.py
+++ variance-standard_deviation-Exercise1-17.py
@@ -15,3 +15,13 @@ minutos,  que toma quedar dormido se obtienen los siguientes datos:
         en la misma línea 
     d)	Comente qué clase de efecto parece tener el hecho de fumar sobre 
         el tiempo que se requiere para quedarse dormido """
+
+from location_measures import average, standard_deviation
+
+if __name__=='__main__':
+    smoking_group = [69.3, 56.0, 22.1, 47.6, 53.2, 48.1, 52.7, 34.4, 60.2, 43.8, 23.2, 13.8]
+    not_smoking_group = [28.6, 25.1, 26.4, 34.9, 29.8, 28.4, 38.5, 30.2, 30.6, 31.8, 41.6, 21.1, 36.0, 37.9, 13.9]
+    print(f'The average of the smoking group is: {average(smoking_group)}')
+    print(f'The standard deviation of the smoking group is: {standard_deviation(smoking_group)}')
+    print(f'\n The average of the smoking group is: {average(not_smoking_group)}')
+    print(f'The standard deviation of the smoking group is: {standard_deviation(not_smoking_group)}')
