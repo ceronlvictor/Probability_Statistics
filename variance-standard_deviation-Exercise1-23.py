613e1fa05740130fccd6283e7943bb6e39893436
Victor Ceron <ceronloezavictor@gmail.com>
2021-01-28 13:15:23 -0600

    Add the problem for variance excersises  1.23 and 1.27

---
+++ variance-standard_deviation-Exercise1-23.py
@@ -0,0 +1,16 @@
+"""En 20 automóviles elegidos aleatoriamente, se tomaron las emisiones 
+de hidrocarburos en velocidad en vacío, en partes por millón (ppm), para 
+modelos de 1980 y 1990. 
+
+    Modelos 1980: 
+        141 359 247 940 882 494 306 210 105 880 200 223 188 940 241 190 300 435 241 380 
+    
+    Modelos de 1990:
+        140 160 20 20 223 60 20 95 360 70 220 400 217 58 235 380 200 175 85 65
+
+    a)Construya un gráfico de puntos como la de la figura 1.1 
+    b)Calcula la media de la muestra para los dos años y sobreponga los dos 
+        medios en los gráficos.
+    c)Comente sobre lo que indica la gráfica de puntos respecto de si cambiar 
+        o no a las emisiones poblacionales de 1980 a 1920. Utilice el concepto de 
+        variabilidad en sus comentarios."""
---
+++ variance-standard_deviation-Exercise1-27.py
@@ -0,0 +1,15 @@
+"""Se realizó un estudio para determinar la influencia del desgaste, y, 
+de un cojinete como una función de la carga, x, sobre el cojinete.  
+Para este estudio se utilizó un diseño experimental con 3 niveles de 
+carga: 700 libras 1000 libras y 1300 libras. En cada nivel se utilizaron 
+4 especímenes y las medias muestrales fueron 210 325 y 375 respectivamente.
+
+    a)Grafique el  promedio de desgaste contra la carga. 
+    b)A partir de la gráfica del inciso a. ¿consideraría que hay una relación 
+        entre el desgaste y la carga?
+    c)Suponga que tenemos los siguientes valores individuales de desgaste para 
+        cada uno de los 4 especímenes en los respectivos niveles de carga (vea 
+        los datos que siguen) grafique los resultados de desgaste para todos los 
+        especímenes contra los 3 valores de carga. 
+    d)A partir de la gráfica del inciso c ¿consideraría que hay una relación clara 
+        si se respuesta difiere del inciso B explique por qué?"""
